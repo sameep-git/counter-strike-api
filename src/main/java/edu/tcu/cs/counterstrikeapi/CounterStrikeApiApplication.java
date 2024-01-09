965a53ab676b47f49e07926f61de39232c4f61fa
sameep-git <sameep.shah@tcu.edu>
2024-01-09 16:19:35 +0530

    initial commit

---
+++ src/main/java/edu/tcu/cs/counterstrikeapi/CounterStrikeApiApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.counterstrikeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterStrikeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterStrikeApiApplication.class, args);
+    }
+
+}
